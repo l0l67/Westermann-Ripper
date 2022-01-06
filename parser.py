748ddd1ed67805df721ede434b77ce5e7e6e07a8
justarandomname <63732527+l0l67@users.noreply.github.com>
2022-01-06 15:06:31 +0100

    cleanup

--- parser.py
+++ parser.py
@@ -5,10 +5,9 @@ import re
 fileData = ""
 urls = []
 
-
 def parserJson():
     global urls, fileData
-    jsonData = json.load(open('json.json', 'r'))
+    jsonData = json.load(open('files.har', 'r'))
     
     counter = 3
     while True:
@@ -25,12 +24,8 @@ def downloadImages():
     for url in urls:
         file_name = re.sub('[^0-9]','', url[-7:]) + '.png'
 
-        download(url, file_name)
-        print('Finished: ' + file_name)
-
-def download(url, file_name):
-    urllib.request.urlretrieve(url, file_name)
-
+        urllib.request.urlretrieve(url, file_name)
+        print('Finished: ' + file_name)    
 
 if __name__ == "__main__":
     parserJson()
