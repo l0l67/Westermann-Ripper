572d4889fd88f48cc6af3705e230470141c92537
justarandomname <63732527+l0l67@users.noreply.github.com>
2022-01-06 15:04:22 +0100

    Uploaded 'parser.py'

---
+++ parser.py
@@ -0,0 +1,37 @@
+import json
+import urllib.request
+import re
+
+fileData = ""
+urls = []
+
+
+def parserJson():
+    global urls, fileData
+    jsonData = json.load(open('json.json', 'r'))
+    
+    counter = 3
+    while True:
+        try:
+            url = jsonData['log']['entries'][counter]['request']['url']
+        except IndexError:
+            break
+        if url.endswith('.png'):
+            urls.append(url)
+        counter += 1
+
+def downloadImages():
+    global urls
+    for url in urls:
+        file_name = re.sub('[^0-9]','', url[-7:]) + '.png'
+
+        download(url, file_name)
+        print('Finished: ' + file_name)
+
+def download(url, file_name):
+    urllib.request.urlretrieve(url, file_name)
+
+
+if __name__ == "__main__":
+    parserJson()
+    downloadImages()
